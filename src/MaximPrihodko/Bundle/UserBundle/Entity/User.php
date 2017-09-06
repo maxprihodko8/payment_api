e4cb135620bc5903f70e8f1bbc5dc4506eb60c01
Maxim Prihodko <max.prihodko8@gmail.com>
2017-09-06 09:37:49 +0300

    User model added and configuration of FOSuser bundle made - changed view for login + config in security

---
+++ src/MaximPrihodko/Bundle/UserBundle/Entity/User.php
@@ -0,0 +1,71 @@
+<?php
+
+namespace MaximPrihodko\Bundle\UserBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+use FOS\UserBundle\Model\User as BaseUser;
+
+/**
+ * User
+ *
+ * @ORM\Table(name="user")
+ * @ORM\Entity(repositoryClass="MaximPrihodko\Bundle\UserBundle\Repository\UserRepository")
+ */
+class User extends BaseUser
+{/**
+     * @var int
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    protected $id;
+
+    /**
+     * @var string
+     * @ORM\Column(name="name", type="string")
+     *
+     */
+    private $name;
+
+    /**
+     * @var int
+     *
+     * @ORM\Column(name="status", type="integer")
+     */
+    private $status;
+
+    /**
+     * Get id
+     *
+     * @return int
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    public function __construct()
+    {
+        parent::__construct();
+        $this->status = 0;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getName()
+    {
+        return $this->name;
+    }
+
+    /**
+     * @param mixed $name
+     */
+    public function setName($name)
+    {
+        $this->name = $name;
+    }
+
+}
+
---
+++ src/MaximPrihodko/Bundle/UserBundle/Service/MessageExecutor.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: pn-user30
+ * Date: 01.09.17
+ * Time: 16:54
+ */
+
+namespace MaximPrihodko\Bundle\UserBundle\Service;
+
+
+class MessageExecutor
+{
+
+}
